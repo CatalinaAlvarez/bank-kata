fa9af0e7998736ebddeb58f6f481aeb7b069a5ca
CatalinaAlvarez <catalina.alvarez051@gmail.com>
2021-11-23 21:06:58 -0500

    Se añaden las funcionalidades del banco

---
+++ src/main/java/Date.java
@@ -0,0 +1,8 @@
+import java.text.SimpleDateFormat;
+
+public class Date {
+
+    public String date(String dateString){
+        return dateString;
+    }
+}
--- src/test/java/AcceptanceTest.java
+++ src/test/java/AcceptanceTest.java
@@ -1,10 +1,4 @@
-import org.junit.Test;
 
 public class AcceptanceTest {
-
-    @Test
-    public void shouldWhatever() {
-
-    }
 
 }
